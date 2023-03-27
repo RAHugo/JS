009c4d65e9f13f191f306193b80d697e72c118ce
Hugo Rojas <rojasaguilarhugo@gmail.com>
2023-03-27 16:35:21 -0600

    Se realizo una pagina html y un javascript para la realización del ejercicio

---
+++ main.js
@@ -0,0 +1,9 @@
+let nombres = ["Sofia","David","Juan"];
+console.log(nombres);
+nombres.shift();
+console.log(nombres);
+nombres.push("Sara","Augustin");
+console.log(nombres);
+
+nombres[1] = "Renata";
+console.log(nombres);
