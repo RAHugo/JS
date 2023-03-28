8aab68ec0014fbb5fa82dea1919d4edfa8cbd731
Hugo Rojas <rojasaguilarhugo@gmail.com>
2023-03-27 21:37:24 -0600

    Realiazción del primer ejercicio y del segundo.

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
+/* Ejercicio 1
+    Parte 1
+*/
 let nombres = ["Sofia","David","Juan"];
 console.log(nombres);
+
+/* Parte 2 */
 nombres.shift();
 console.log(nombres);
 nombres.push("Sara","Augustin");
 console.log(nombres);
 
-nombres[1] = "Renata";
+/* Parte 3 */
+nombres.slice(1,0,"Renata");
 console.log(nombres);
+nombres.push("Elena");
+
+for(let index = 0; index<nombres.length; index++){
+    console.log(nombres[index]);
+}
+
+
+/* Ejercicio 2 */
+
+let arbol = [];
+let contArbol = "*";
+
+for(i = 0; i<5; i +=1){
+  arbol[i] = contArbol;
+  contArbol += "*";
+  console.log(arbol[i]);
+}
+
+
+/* Ejercicio 3 */
